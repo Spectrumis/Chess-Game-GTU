f3c8d510d403522d471a5d09c83d724f779ab722
recep sivri <recep.sivri.eskipazar@gmail.com>
2016-12-10 18:52:50 +0300

    path fixed

--- Path/src/path/Path.java
+++ Path/src/path/Path.java
@@ -9,6 +9,8 @@ package path;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.swing.text.html.HTML.Tag.HEAD;
+
 /**
  *
  * @author Selman Ahatlı
@@ -277,18 +279,6 @@ public class Path {
             _20(false,path);
         }    
     }
-    
-<<<<<<< HEAD
-
-    
-    public static void main(String[] args) {
-        
-        List<String> path = new ArrayList<>();
-       
-        path.add("1-");
-        _2(true,path);
-        _2(false,path);
-    }
 
     public static void _42(boolean value, List<String> path){
 
@@ -499,8 +489,8 @@ public class Path {
         {
             path.add("(T)-");
             path.add("70-");
-            _72(true,path);
-            _72(false,path);
+            _111(true,path);
+            _111(false,path);
         }
         else
         {
@@ -546,8 +536,8 @@ public class Path {
         else
         {
             path.add("(F)-");
-            _69(true,path);
-            _69(false,path);
+            _111(true,path);
+            _111(false,path);
         }
     }
     public static void _76(boolean value, List<String> path){
@@ -570,15 +560,9 @@ public class Path {
         }
     }
     public static void _5(boolean value, List<String> path){
-
-        path.add("5");
-
-=======
-    public static void _5(boolean value, List<String> path){
         
         path.add("5");
-        
->>>>>>> origin/master
+
         if(value)
         {
             path.add("(T)-");
@@ -591,31 +575,18 @@ public class Path {
             path.add("(F)-");
             _2(true,path);
             _2(false,path);
-<<<<<<< HEAD
         }
-    }
-
-    public static void _7(boolean value, List<String> path){
-
-        path.add("7");
-
-=======
-        }    
     }
     
     public static void _7(boolean value, List<String> path){
         
         path.add("7");
-        
->>>>>>> origin/master
+
         if(value)
         {
             path.add("(T)-");
             path.add("8-");
-<<<<<<< HEAD
             printList(path);
-=======
->>>>>>> origin/master
             _9(true,path);
             _9(false,path);
         }
@@ -623,26 +594,15 @@ public class Path {
         {
             path.add("(F)-");
             path.add("13-");
-<<<<<<< HEAD
             printList(path);
             _14(true,path);
             _14(false,path);
         }
     }
     public static void _2(boolean value, List<String> path) {
-
-        path.add("2");
-
-=======
-            _14(true,path);
-            _14(false,path);
-        }  
-    }
-    public static void _2(boolean value, List<String> path) {
         
         path.add("2");
-        
->>>>>>> origin/master
+
         if(value)
         {
             path.add("(T)-");
@@ -658,38 +618,307 @@ public class Path {
             _7(false,path);
         }
     }
-<<<<<<< HEAD
 
-
-
-    public static void printList(List<String> path)
-    {
-        for (int i = 0; i < path.size(); ++i) {
-
-            System.out.print(path.get(i));
-
-        }
-
-=======
-    
     public static void main(String[] args) {
-        
+
         List<String> path = new ArrayList<>();
-       
+
         path.add("1-");
         _2(true,path);
         _2(false,path);
     }
-    
+
     public static void printList(List<String> path)
     {
         for (int i = 0; i < path.size(); ++i) {
-            
+
             System.out.print(path.get(i));
-            
+
         }
-        
->>>>>>> origin/master
+
         System.out.println("");
+    }
+
+    public static void _79(boolean value, List<String> path) {
+
+        path.add("79");
+
+        if(value)
+        {
+            path.add("(T)-");
+            path.add("85-");
+            _86(true,path);
+            _86(false,path);
+        }
+        else
+        {
+            path.add("(F)-");
+            path.add("88-");
+            _89(true,path);
+            _89(false,path);
+        }
+    }
+
+    public static void _89(boolean value, List<String> path) {
+
+        path.add("89");
+
+        if(value)
+        {
+            path.add("(T)-");
+            path.add("90-");
+            _91(true,path);
+            _9(false,path);
+        }
+        else
+        {
+            path.add("(F)-");
+            path.add("93-");
+            _94(true,path);
+            _94(false,path);
+        }
+    }
+    public static void _86(boolean value, List<String> path) {
+
+        path.add("86");
+
+        if(value)
+        {
+            path.add("(T)-");
+            path.add("87-");
+            path.add("88-");
+            _89(true,path);
+            _89(false,path);
+        }
+        else
+        {
+            path.add("(F)-");
+            _79(true,path);
+            _79(false,path);
+        }
+    }
+    public static void _80(boolean value, List<String> path) {
+
+        path.add("80");
+
+        if(value)
+        {
+            path.add("(T)-");
+            path.add("81-");
+            _82(true,path);
+            _82(false,path);
+        }
+        else
+        {
+            path.add("(F)-");
+            path.add("84-");
+            path.add("88-");
+            _89(true,path);
+            _89(false,path);
+        }
+    }
+    public static void _82(boolean value, List<String> path) {
+
+        path.add("82");
+
+        if(value)
+        {
+            path.add("(T)-");
+            path.add("83-");
+            path.add("84-");
+            path.add("88-");
+            _89(true,path);
+            _89(false,path);
+        }
+        else
+        {
+            _80(true,path);
+            _80(false,path);
+        }
+    }
+    public static void _91(boolean value, List<String> path) {
+
+        path.add("91");
+
+        if(value)
+        {
+            path.add("(T)-");
+            path.add("92-");
+            path.add("93-");
+            _94(true,path);
+            _94(false,path);
+        }
+        else
+        {
+            path.add("(F)-");
+            _103(true,path);
+            _103(false,path);
+        }
+    }
+    public static void _94(boolean value, List<String> path) {
+
+        path.add("94");
+
+        if(value)
+        {
+            path.add("(T)-");
+            path.add("95-");
+            _100(true,path);
+            _100(false,path);
+        }
+        else
+        {
+            path.add("(F)-");
+            _101(true,path);
+            _101(false,path);
+        }
+    }
+
+    public static void _96(boolean value, List<String> path) {
+
+        path.add("96");
+
+        if(value)
+        {
+            path.add("(T)-");
+            path.add("97-");
+            _98(true,path);
+            _98(false,path);
+        }
+        else
+        {
+            path.add("(F)-");
+            _94(true,path);
+            _94(false,path);
+        }
+    }
+    public static void _98(boolean value, List<String> path) {
+
+        path.add("98");
+
+        if(value)
+        {
+            path.add("(T)-");
+            path.add("99-");
+            _100(true,path);
+            _100(false,path);
+        }
+        else
+        {
+            path.add("(F)-");
+            _100(true,path);
+            _100(false,path);
+        }
+    }
+    public static void _100(boolean value, List<String> path) {
+
+        path.add("100");
+
+        if(value)
+        {
+            path.add("(T)-");
+            _101(true,path);
+            _101(false,path);
+        }
+        else
+        {
+            path.add("(F)-");
+            _111(true,path);
+            _111(false,path);
+        }
+    }
+    public static void _101(boolean value, List<String> path) {
+
+        path.add("101");
+
+        if(value)
+        {
+            path.add("(T)-");
+            path.add("102-");
+            path.add("105-");
+            _107(true,path);
+            _107(false,path);
+        }
+        else
+        {
+            path.add("(F)-");
+            _103(true,path);
+            _103(false,path);
+        }
+    }
+
+    public static void _103(boolean value, List<String> path) {
+
+        path.add("103");
+
+        if(value)
+        {
+            path.add("(T)-");
+            path.add("104-");
+            path.add("105-");
+            _107(true,path);
+            _107(false,path);
+        }
+        else
+        {
+            path.add("(F)-");
+            path.add("105-");
+            _107(true,path);
+            _107(false,path);
+        }
+    }
+    public static void _107(boolean value, List<String> path) {
+
+        path.add("107");
+
+        if(value)
+        {
+            path.add("(T)-");
+            path.add("106-");
+            _111(true,path);
+            _111(false,path);
+        }
+        else
+        {
+            path.add("(F)-");
+            path.add("110-");
+            _111(true,path);
+            _111(false,path);
+        }
+    }
+    public static void _108(boolean value, List<String> path) {
+
+        path.add("108");
+
+        if(value)
+        {
+            path.add("(T)-");
+            path.add("109-");
+            path.add("110-");
+            _111(true,path);
+            _111(false,path);
+        }
+        else
+        {
+            path.add("(F)-");
+            _111(true,path);
+            _111(false,path);
+        }
+    }
+    public static void _111(boolean value, List<String> path) {
+
+        path.add("111");
+
+        if(value)
+        {
+            path.add("(T)-");
+            path.add("112-");
+            path.add("113");
+        }
+        else
+        {
+            path.add("(F)-");
+            path.add("113");
+        }
     }
 }
